ecdfc44184351f670f5a20a577f8119174960214
vanhstp <vanhstp@users.noreply.github.com>
2022-08-28 07:27:26 +0000

    deploy: 471e955a2bb9445989b12f9d75499d46fcba49c5

---
+++ assets/about_index.md.387f5bad.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c as a}from"./app.17ab7e60.js";const m=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"about/index.md"}'),o={name:"about/index.md"};function n(r,s,c,d,i,p){return t(),a("div")}const f=e(o,[["render",n]]);export{m as __pageData,f as default};
